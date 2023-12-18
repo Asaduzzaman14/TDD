21e10348a5a4c4d2dd7322fa479b01650516383b
Md Asaduzaman <asadasad9282@gmail.com>
2023-12-18 15:50:55 +0600

    cjanges

--- hooks.test.js
+++ hooks.test.js
@@ -1,4 +1,4 @@
-import { beforeEach, expect, it, test } from "vitest";
+import { beforeAll, beforeEach, describe, expect, it, test } from "vitest";
 import { User } from "./hooks";
 
 const testEmail = "test@gmail.com";
@@ -8,27 +8,36 @@ beforeEach(() => {
   user = new User(testEmail);
 });
 
-it("should have an email property", () => {
-  expect(user).toHaveProperty("email");
-});
+beforeAll()
 
-it("should update the email property", () => {
-  const newTestEmail = "test2@gmail.com";
-  user.updateEmail(newTestEmail);
+describe('test',() => {
+  beforeAll()
 
-  expect(user.email).toBe(newTestEmail);
-});
+  it("should have an email property", () => {
+    expect(user).toHaveProperty("email");
+  });
+  
+  it("should update the email property", () => {
+    const newTestEmail = "test2@gmail.com";
+    user.updateEmail(newTestEmail);
+  
+    expect(user.email).toBe(newTestEmail);
+  });
+  
+})
 
-it("should store the provided email value", () => {
-  expect(user.email).toBe(testEmail);
-});
 
-it("should clear the email", () => {
-  user.clearEmail();
-  expect(user.email).toBe("");
-});
-
-it("should should have an email property after clearing the email", () => {
-  user.clearEmail();
-  expect(user).toHaveProperty("email");
-});
+  it.concurrent("should store the provided email value", () => {
+    expect(user.email).toBe(testEmail);
+  });
+  
+  it("should clear the email", () => {
+    user.clearEmail();
+    expect(user.email).toBe("");
+  });
+  
+  it("should should have an email property after clearing the email", () => {
+    user.clearEmail();
+    expect(user).toHaveProperty("email");
+  });
+  
